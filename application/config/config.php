527feaeb4daf3fa85a9baf21c3d4e53ada829cbf
Jake <ljangwon@gmail.com>
2021-06-01 23:19:28 +0900

    init

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'dnffkffk';
+$config['encryption_key'] = 'aw2q132141545123^%12an?@kfklsfs';
 
 /*
 |--------------------------------------------------------------------------
